57a064cd027603e19261139fd613d05578e48e05
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-16 23:03:58 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function Select_and_Remove() {
+            var selectElement = document.getElementById('colorSelect');
+            var selectedItem = selectElement.options[selectElement.selectedIndex].value;
+	document.getElementById(selectedItem).remove();
+}
+
+document.getElementById("select_remove").addEventListener("click",Select_and_Remove);
